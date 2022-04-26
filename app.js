53cad4a26c8ad146aa87582a582c4018158e3b93
Gustavo Lira <gu.lirar23@gmail.com>
2022-04-26 15:04:24 -0500

    add: config to use express.static to response a html file

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ const app = express();
 //Puerto en que se verá la aplicación: localhost:3000
 const port = 3000;
 
+//tring give an html file as server response with express.static
+app.use(express.static('public'));
+
 //path inicial, responderá a la url localhost:3000
 app.get('/',(req,res) => {
     res.send('Hello World!')
@@ -16,7 +19,7 @@ app.get('/',(req,res) => {
 //localhost:3000/launchx
 app.get('/launchx', (req, res) => {
     res.send('Bienvenidos a launchX')
-})
+});
 
 //Agregar nueva ruta y regresar objeto
 //localhost:3000/explorersInNode
@@ -26,15 +29,16 @@ app.get('/explorersInNode', (req, res) => {
         msg: 'Hola'
     }
     res.send(explorer)
-})
+});
 
 //Query params: Receive parameters using url
 //localhost:3000/explorers/john
 app.get('/explorer/:explorerName', (req, res) => {
     res.send(req.params)
-})
+});
+
 
 //Para inicializar la app
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
+    console.log(`Example app listening on port: ${port}`)
+});
